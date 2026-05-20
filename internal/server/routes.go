2bd3974c05a3b92af84ca73007efbe26241c3017
Lukas Schneider <schneiderlukas197@gmail.com>
2026-05-20 19:54:15 +0200

    feat: added routes for each endpoint for 1.6.1

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,20 +7,71 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
+	r.Route("/operator", func(r chi.Router) {
+		r.Get("/ping", s.HelloWorldHandler)
+		r.Get("/meta", s.HelloWorldHandler)
+		r.Get("/stations", s.HelloWorldHandler)
+		r.Get("/available-assets", s.HelloWorldHandler)
+		r.Get("/alerts", s.HelloWorldHandler)
+		r.Get("/operating-calender", s.HelloWorldHandler)
+		r.Get("/operating-hours", s.HelloWorldHandler)
+		r.Get("/information", s.HelloWorldHandler)
+		r.Get("/pricing-plans", s.HelloWorldHandler)
+		r.Get("/regions", s.HelloWorldHandler)
+	})
+
+	r.Route("/planning", func(r chi.Router) {
+		r.Post("/inquiries", s.HelloWorldHandler)
+		r.Post("/offers", s.HelloWorldHandler)
+	})
+
+	r.Route("/bookings", func(r chi.Router) {
+		r.Post("/", s.HelloWorldHandler)
+		r.Post("/one-stop", s.HelloWorldHandler)
+		r.Post("/{id}/events", s.HelloWorldHandler)
+		r.Get("/{id}", s.HelloWorldHandler)
+		r.Get("/", s.HelloWorldHandler)
+		r.Put("/{id}", s.HelloWorldHandler)
+		r.Delete("/{id}/subscriptions", s.HelloWorldHandler)
+		r.Post("/{id}/subscriptions", s.HelloWorldHandler)
+		r.Post("/{id}/notifications", s.HelloWorldHandler)
+		r.Get("/{id}/notifications", s.HelloWorldHandler)
+	})
+
+	r.Route("/legs", func(r chi.Router) {
+		r.Get("/legs/{id}/available-assets", s.HelloWorldHandler)
+		r.Get("/{id}", s.HelloWorldHandler)
+		r.Put("/{id}", s.HelloWorldHandler)
+		r.Post("/{id}/ancillaries/{category/{number}", s.HelloWorldHandler)
+		r.Delete("/{id}/ancillaries/{category/{number}", s.HelloWorldHandler)
+		r.Post("/{id}/events", s.HelloWorldHandler)
+		r.Get("/{id}/progress", s.HelloWorldHandler)
+		r.Post("/{id}/progress", s.HelloWorldHandler)
+		r.Post("/{id}/confirmation", s.HelloWorldHandler)
+	})
+
+	r.Route("/payment", func(r chi.Router) {
+		r.Get("/journal-entry", s.HelloWorldHandler)
+		r.Get("/{id}/claim-extra-costs", s.HelloWorldHandler)
+	})
+
+	r.Route("/support", func(r chi.Router) {
+		r.Get("/", s.HelloWorldHandler)
+		r.Get("/{id}/status", s.HelloWorldHandler)
+	})
+
+	r.Route("/customers", func(r chi.Router) {
+		r.Post("/", s.HelloWorldHandler)
+		r.Patch("/{id}", s.HelloWorldHandler)
+		r.Get("/{id}", s.HelloWorldHandler)
+		r.Delete("/{id}", s.HelloWorldHandler)
+	})
 
 	r.Get("/", s.HelloWorldHandler)
 
